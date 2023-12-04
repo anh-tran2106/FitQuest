920f20bd2874dcbb70cc0ff60aaa03a5be011832
Anh Tran <tranlamtuananh2003@gmail.com>
2023-12-04 04:06:51 -0400

    graph for sleep now fetch data from database

--- app/src/main/java/ca/unb/mobiledev/fitquest/RecordSleepActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/fitquest/RecordSleepActivity.kt
@@ -33,6 +33,8 @@ class RecordSleepActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_sleep)
 
+        val username = intent.getStringExtra("username")!!
+
         val picker: nl.joery.timerangepicker.TimeRangePicker = findViewById(R.id.timeRangePicker)
         picker.setOnTimeChangeListener(object : TimeRangePicker.OnTimeChangeListener {
             override fun onStartTimeChange(startTime: TimeRangePicker.Time) {
@@ -58,36 +60,40 @@ class RecordSleepActivity : AppCompatActivity() {
                 // Do something on stop dragging
             }
         })
+
+        sendDataToScreen(username)
+        handleSetTimeClick()
     }
     private fun sendDataToScreen(username: String) {
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val monthDateFormatter = DateTimeFormatter.ofPattern("M/d")
         val currentTimeNoFormat = LocalDate.now()
+        val currentTime = LocalDate.now().format(formatter)
         val userRef = db.collection("users").document(username)
 
-        val barChart = findViewById<BarChart>(R.id.waterChart)
+        val barChart = findViewById<BarChart>(R.id.sleepChart)
         val dataList: ArrayList<BarEntry> = ArrayList()
         val xAxisLables: MutableList<String> = ArrayList()
-        var sleepHours: Float
+        var stepCounter: Float
         var counter = 0
         userRef.get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 val documentSnapshot = task.result
                 if (documentSnapshot.exists()) {
 
-                    // Update water data for 7 days up to the current date to the chart
+                    // Update step data for 7 days up to the current date to the chart
                     for (i in 6 downTo 0) {
-                        sleepHours = 0f
+                        stepCounter = 0f
                         if ((documentSnapshot.data?.get("allDays") as (HashMap<*, *>))[currentTimeNoFormat.minusDays(i.toLong()).format(formatter)] != null) {
-                            sleepHours = ((documentSnapshot.data?.get("allDays") as (HashMap<*, *>))[currentTimeNoFormat.minusDays(i.toLong()).format(formatter)] as (HashMap<*, *>))["waterCounter"].toString().toFloat()
+                            stepCounter = ((documentSnapshot.data?.get("allDays") as (HashMap<*, *>))[currentTimeNoFormat.minusDays(i.toLong()).format(formatter)] as (HashMap<*, *>))["sleepTime"].toString().toFloat()
                         }
-                        dataList.add(BarEntry(counter.toFloat(), sleepHours))
+                        dataList.add(BarEntry(counter.toFloat(), stepCounter))
                         xAxisLables.add(currentTimeNoFormat.minusDays(i.toLong()).format(monthDateFormatter))
                         counter++;
                     }
 
-                    val barDataSet = BarDataSet(dataList,"Hours of Sleep")
-                    barDataSet.valueFormatter = DefaultValueFormatter(0)
+                    val barDataSet = BarDataSet(dataList,"Steps")
+                    barDataSet.valueFormatter = DefaultValueFormatter(2)
                     barDataSet.color = Color.rgb(65, 75, 178)
                     val barData = BarData(barDataSet)
 
@@ -98,6 +104,39 @@ class RecordSleepActivity : AppCompatActivity() {
                     barChart.animateY(700, Easing.EaseOutSine)
                     barChart.setTouchEnabled(false)
                 }
+            }
+        }
+    }
+
+    private fun handleSetTimeClick() {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val currentTime = LocalDate.now().format(formatter)
+
+        val setTimeButton: Button = findViewById(R.id.setTimeBtn)
+        val timeRangePicker: nl.joery.timerangepicker.TimeRangePicker = findViewById(R.id.timeRangePicker)
+        val username = intent.getStringExtra("username")!!
+
+        val userRef = db.collection("users").document(username)
+
+        setTimeButton.setOnClickListener {
+            userRef.get().addOnCompleteListener {
+                userRef
+                    .update(
+                        "allDays.${currentTime}.sleepTime", timeRangePicker.durationMinutes / 60.0
+                    )
+                    .addOnSuccessListener {
+                        Toast.makeText(this@RecordSleepActivity, "Sleep Time Set", Toast.LENGTH_SHORT).show()
+
+                        sendDataToScreen(username)
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(
+                            this@RecordSleepActivity,
+                            "Error while setting sleep time",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Log.d(ContentValues.TAG, e.toString())
+                    }
             }
         }
     }
